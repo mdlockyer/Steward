f63006daccb63e456de985c636bae6923823f59a
Michael Lockyer <mdlockyer@gmail.com>
2026-04-20 12:53:55 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 6.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUITemplates",
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "SwiftUITemplates"
+        ),
+
+    ],
+    swiftLanguageModes: [.v6]
+)
